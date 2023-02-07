9fe17437d236671fb4b391eacf4b8dd63bd408ae
yujin5004 <yujin5004@netand.co.kr>
2023-02-07 17:57:38 +0900

    0 Tutorial

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,30 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split(){
+        String [] test = "1,2".split(",");
+        String [] result = {"2","1"};
+        String [] test2 = "1".split(",");
+        String [] result2 = {"1"};
+        assertThat(test).contains("2","2");
+        assertThat(test2).containsExactly("1");
+    }
+
+    @Test
+    void subStr(){
+        String test = "(1,2)".substring(1,4);
+        assertThat(test).isEqualTo("1,2");
+    }
+
+    @Test
+    void split3(){
+        String [] test = "1,2".split(",");
+        String [] result = {"2","1"};
+        String [] test2 = "1".split(",");
+        String [] result2 = {"1"};
+        assertThat(test).contains("2","2");
+        assertThat(test2).containsExactly("1");
+    }
 }
