8aabc629663aafcc97f8c535ffd3da85166ce923
yujin5004 <yujin5004@netand.co.kr>
2023-03-16 17:38:11 +0900

    feature: create Validation Method isThatThreeNumber, number1to9Vali, numberDuplicationValidation

--- src/test/java/ValidationTest.java
+++ src/test/java/ValidationTest.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class ValidationTest {
+import baseball.ValidationUnit;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ValidationTest {
+    @Test
+    void isThisThreeNumber() {
+        assertThat(ValidationUnit.isThatThreeNumber(123)).isTrue();
+        assertThat(ValidationUnit.isThatThreeNumber(12)).isFalse();
+        assertThat(ValidationUnit.isThatThreeNumber(1244)).isFalse();
+    }
+
+    @Test
+    void thisNumberIs1to9() {
+        assertThat(ValidationUnit.number1to9Vali(1)).isTrue();
+        assertThat(ValidationUnit.number1to9Vali(0)).isFalse();
+        assertThat(ValidationUnit.number1to9Vali(10)).isFalse();
+    }
+
+    @Test
+    void duplicationTest() {
+        assertThat(ValidationUnit.numberDuplicationValidation(123)).isTrue();
+    }
 }
--- src/test/java/baseball/ValidationUnit.java
+++ src/test/java/baseball/ValidationUnit.java
@@ -1,2 +1,27 @@
-package baseball;public class ValidationUnit {
+package baseball;
+
+public class ValidationUnit {
+
+    private static final int SCORE_LENGTH = 3;
+    private static final int MAX_NO = 9;
+    private static final int MIN_NO = 1;
+
+    public static boolean isThatThreeNumber(int number) {
+        if((int)(Math.log10(number)+1)==SCORE_LENGTH){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean number1to9Vali(int inputNumber) {
+        if(inputNumber<=MAX_NO && inputNumber>=MIN_NO){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean numberDuplicationValidation(int i) {
+
+        return false;
+    }
 }
